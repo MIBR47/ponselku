dda7b4aa4fc1ef600d0b344bc949f58d4e955071
mib0208 <ilhambachtiar47@gmail.com>
2019-07-14 17:05:33 +0700

    news

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,9 +1,11 @@
 class NewsController < ApplicationController
+	http_basic_authenticate_with name: "admin", password: "secret", except: [:index, :show]
 	def index
 		@news = New.all
 	end
+
 	def show
-    @new = New.find(params[:id])
+    	@new = New.find(params[:id])
   	end
 
 	def new
@@ -11,13 +13,28 @@ class NewsController < ApplicationController
 
 	def create
 	  @new = New.new(new_params)
-	 
+	  @new.image.attach(new_params[:image])
 	  @new.save
 	  redirect_to @new
+	end
+
+	def edit
+		@new = New.find(params[:id])
+
+	end
+
+	def update
+	  	@new = New.find(params[:id])
+	 	@new.image.attach(new_params[:image])
+	  	if @new.update(new_params)
+	    	redirect_to @new
+	  	else
+	    	render 'edit'
+	  	end
 	end
  
 private
 	def new_params
-		params.require(:new).permit(:title, :text)
+		params.require(:new).permit(:image, :title, :text)
 	end
 end
